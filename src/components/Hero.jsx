0847a5f282378be1ac751c57ea6f04483d0d1bbb
Bartlomiej Calko <bartlomiejcalko@brainZero.local>
2024-11-30 12:54:10 +0100

    initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+      <div className='relative h-dvh w-screen overflow-x-hidden'>
+          <div>
+              
+          </div>
+    </div>
+  )
+}
+
+export default Hero
