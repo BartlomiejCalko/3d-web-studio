27ce7babb74c0dab6be3ac209c32ea91e44024bb
Bartlomiej Calko <bartlomiejcalko@brainZero.local>
2024-12-01 17:22:42 +0100

    About section with animated title. Navbar start

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Hero from './components/Hero'
+import About from './components/About'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Navbar />
       <Hero />
-
+      <About /> 
       <section className='z-0 min-h-screen bg-blue-500'/>
       <section className='z-0 min-h-screen bg-pink-500'/>
     </main>
